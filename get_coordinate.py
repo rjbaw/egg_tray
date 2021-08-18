debug messages and resolution scaling fix

get_coordinate.py
@@ -1,22 +1,24 @@
 import argparse
 from sys import platform
-from models import *  
+from models import *  # set ONNX_EXPORT in models.py
 from utils.datasets import *
 from utils.utils import *
-#from utils.basler import *
 import numpy as np
 import time
 import cv2
+import dlib
 import torch
+import torchvision
 import json
 import socket
+from scipy import spatial
 from scipy.spatial.transform import Rotation
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element, SubElement
 
 class Detect(object):
     def __init__(self, opt):
-        self.img_size = opt.img_size  
+        self.img_size = opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
         self.out = opt.output
         self.source = opt.source
         self.weights = opt.weights
@@ -30,25 +32,32 @@ class Detect(object):
         self.nms_thres = opt.conf_thres
         self.undistort = opt.undistort
         self.use_socket = opt.disable_socket
-        self.robot = opt.robot
+        self.save_img = opt.save
+        self.debug = opt.silent
         self.vid_path = None
         self.vid_writer = None
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         self.fps = 0
-        self.cartesian_xcoor = 0.017 
-        self.cartesian_ycoor = 0.0096 
         self.load_camera_parameters(opt.camera_params)
         if self.use_socket:
             self.socket_connection(opt.test_socket)
 
+        self.robot = 'KUKA'
+        self.cartesian_xcoor = 0.017 
+        self.cartesian_ycoor = 0.0096 
+
     def load_camera_parameters(self, cam_params_path):
+        if self.debug:
+            print('LOADING CAMERA PARAMETERS.......')
         with open(cam_params_path, 'rb') as f:
             self.rms = np.load(f)
             self.camera_matrix = np.load(f)
             self.dist_coefs = np.load(f)
-            print('rms: \n', self.rms)
-            print('camera_matrix: \n', self.camera_matrix)
-            print('dist_coefs: \n', self.dist_coefs)
+            print('RMS: \n', self.rms)
+            print('CAMERA MATRIX: \n', self.camera_matrix)
+            print('DISTORTION COEFFICIENTS: \n', self.dist_coefs)
+        if self.debug:
+            print('CAMERA PARAMETERS LOADED')
 
     def socket_connection(self, test):
         if test:
@@ -57,14 +66,14 @@ class Detect(object):
         else:
             PORT = 54600
             IP = '192.168.1.10'
-        print('ADDRESS: tcp://{}:{}'.format(IP, PORT))
+        print('ADDRESS: TCP://{}:{}'.format(IP, PORT))
+        print("CONNECTING......")
         while True:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.settimeout(5)
             try:
-                print("connecting....")
                 self.sock.connect((IP, PORT))
-                print("connected.")
+                print("CONNECTED.")
                 break
             except Exception as e:
                 print(e)
@@ -83,23 +92,29 @@ class Detect(object):
                 print(e)
                 time.sleep(1)
         if len(robot_data) == 0:
+            print('RECIEVED EMPTY DATA, RETRYING CONNECTION.....')
             return self.socket_connection(False)
         else:
             return self.read_robot_data(robot_data)
 
     def read_robot_data(self, robot_data):
         if self.robot == 'ABB':
+            if self.debug:
+                print('ABB ROBOT')
             robot_data = self.read_json(robot_data)
         elif self.robot == 'KUKA':
+            if self.debug:
+                print('KUKA ROBOT')
             robot_data = self.read_xml(robot_data)
         else:
-            assert True, 'Robot not supported'
-        print(robot_data)
+            assert True, 'ROBOT TYPE NOT SUPPORTED'
+        if debug:
+            print('STATE: ', robot_data)
 
     def read_json(self, robot_data):
         rd = json.loads(robot_data)
         state = rd[0]['mode']
-        print(state)
+        print('RECIEVED: ', state)
         if 0 < float(state) < 10 :
             return 1 
         elif 10 < float(state) < 20:
@@ -113,7 +128,7 @@ class Detect(object):
 
     def read_xml(self, robot_data):
         data = ElementTree.fromstring(robot_data)
-        print(data)
+        print('RECIEVED: ', data)
         data = float(data[0].attrib['Trig'])
         print(data)
         if 0 < data < 10:
@@ -128,25 +143,29 @@ class Detect(object):
             return 0 
 
     def get_sources(self):
+        if self.debug:
+            print('CONFIGURING SOURCES....')
         try:
             int(self.source)
             self.webcam = True
         except:
             self.webcam = self.source.startswith('rtsp') or self.source.startswith('http') or self.source.endswith('.txt') or self.source.startswith('v4l2src') or self.source.startswith('basler')
         if self.webcam:
-            self.save_img = False
+            if self.debug:
+                print('USING STREAM SOURCES...')
             torch.backends.cudnn.benchmark = True  
-            if self.source.startswith('basler'):
-                dataset = BaslerCamera(self.source, img_size=None, half=self.half)
-            else:
-                dataset = LiveFeed(self.source, half=self.half)
+            dataset = LiveFeed(self.source, half=self.half)
         else:
-            self.save_img = True
-            self.view_img = False
+            if self.debug:
+                print('USING MEDIA FILES SOURCES....')
             dataset = MediaFiles(self.source, half=self.half)
+        if self.debug:
+            print('SOURCES CONFIGURED')
         return dataset
 
     def load_model(self, device):
+        if self.debug:
+            print('LOADING MODEL....')
         model = Darknet(self.cfg, self.img_size)
         if self.weights.endswith('.pt'): 
             model.load_state_dict(torch.load(self.weights, map_location=device)['model'])
@@ -156,6 +175,8 @@ class Detect(object):
         self.half = self.half and device.type != 'cpu'  
         if self.half:
             model.half()
+        if self.debug:
+            print('LOAD MODEL COMPLETED')
         return model
 
     def initialize_output(self):
@@ -172,82 +193,89 @@ class Detect(object):
             img = [letterbox(x, new_shape=self.img_size, interp=cv2.INTER_LINEAR)[0] for x in im0]
             img = np.stack(img, 0)
             img = img[:, :, :, ::-1].transpose(0, 3, 1, 2)  # BGR to RGB
-            img = np.ascontiguousarray(img, dtype=np.float16 if self.half else np.float32)  # uint8 to fp16/fp32
+            img = np.ascontiguousarray(img, dtype=np.float16 if self.half else np.float32) 
             img /= 255.0  
         else:
             img = letterbox(im0, new_shape=self.img_size)[0]
             img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB
-            img = np.ascontiguousarray(img, dtype=np.float16 if self.half else np.float32)  # uint8 to fp16/fp32
+            img = np.ascontiguousarray(img, dtype=np.float16 if self.half else np.float32)
             img /= 255.0 
         img = torch.from_numpy(img).to(device)
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
         return img
 
+    def scale_detection_box(self, data):
+        x, y, xmax, ymax = data
+        x = x/self.width * 1920
+        xmax = xmax/self.width * 1920
+        y = y/self.height * 1080
+        ymax = ymax/self.height * 1080
+        return (x,y,xmax,ymax)
+
     def detect(self, img, model, colors):
+        if self.debug:
+            print('DETECTING IMAGE....')
         g_rects = []
-        count = 0
         pred = model(img)[0]
         if self.half:
             pred = pred.float()
         pred = non_max_suppression(pred, self.conf_thres, self.nms_thres)
         for i, det in enumerate(pred):
-            self.real_count = 0
             if det is not None and len(det):
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], self.im0.shape).round()
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()
-                    self.real_count += int(n)
                 for *xyxy, conf, _, cls in det:
                     if self.save_img or self.view_img: 
                         label = '%s %.2f' % (self.classes[int(cls)], conf)
                         plot_one_box(xyxy, self.im0, label=label, color=colors[int(cls)])
-                    x, y, xmax, ymax = xyxy
+                    x, y, xmax, ymax = self.scale_detection_box(xyxy)
                     g_rects.append([int(x), int(y), int(xmax), int(ymax), float(conf), int(cls)])
-        return np.array(g_rects)
+        g_rects = np.array(g_rects)
+        if self.debug:
+            print('RESULTS: ')
+            print(g_rects)
+        return g_rects
 
-    def put_status_bar(self, t0):
+    def put_status_bar(self):
         cv2.rectangle(self.im0, (0, 0), (self.width, int(self.height/20.0)), (255, 255, 255), -1)
         cv2.putText(self.im0, 'FPS: %.1f' % (self.fps), (self.width - 112, 20), self.font, 0.4, (255, 0, 0), 1)
-        cv2.putText(self.im0, 'Real-time Count: ' + str(self.real_count), (10, 20), self.font, 0.4, (255, 0, 0), 1)
 
     def save_video(self, vid_cap):
+        self.save_path = os.path.join(self.out,'sample.mp4')
         if self.vid_path != self.save_path:  
             self.vid_path = self.save_path
             if isinstance(self.vid_writer, cv2.VideoWriter):
                 self.vid_writer.release()  
-            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            self.vid_writer = cv2.VideoWriter(self.save_path, cv2.VideoWriter_fourcc(*self.fourcc), 30, (w, h))
+            h,w = self.im0.shape[:2]
+            self.vid_writer = cv2.VideoWriter(self.save_path,
+                                              cv2.VideoWriter_fourcc(*self.fourcc),
+                                              30, (w, h))
         self.vid_writer.write(self.im0)
 
     def save_to_out(self, mode, vid_cap):
-        if mode == 'images':
-            cv2.imwrite(self.save_path, self.im0)
-        else:
-            self.save_video(vid_cap)
-
-    def get_xedge_pos(self, g_rects):
-        index_max = np.argmax(g_rects[:,2])
-        return g_rects[index_max]
+        #cv2.imwrite(self.save_path, self.im0)
+        self.save_video(vid_cap)
 
     def get_xyedge_pos(self, g_rects):
         if len(g_rects) == 1:
-            return np.squeeze(g_rects, axis=0), True
+            return np.squeeze(g_rects, axis=0)
         else:
             x_idx = np.argmax(g_rects[:,2])
             x_cluster = g_rects[abs(g_rects[x_idx][2] - g_rects[:,2]) < 0.2]
             xy_idx = np.argmin(x_cluster[:,3])
-        return x_cluster[xy_idx], False
-
-    def get_centroid_pos(self, rect):
-        return (rect[0] + rect[2])/2.0, (rect[1] + rect[4])/2.0
+        ret = x_cluster[xy_idx]
+        print('SELECTED COORDINATES: ', ret)
+        return ret
 
     def transform_pnp(self, rect):
+        if self.debug:
+            print('CALCULATING COORDINATES.....')
         planar_coordinate = self.get_planar_coordinate()
         camera_coordinate = self.get_camera_coordinate(rect)
         ret, rvec, tvec = cv2.solvePnP(planar_coordinate, camera_coordinate, self.camera_matrix, self.dist_coefs)
+        if self.debug:
+            print('ROTATION VECTOR: \n', rvec)
+            print('TRANSLATION VECTOR: \n', tvec)
         return rvec, tvec
 
     def get_planar_coordinate(self):
@@ -269,6 +297,8 @@ class Detect(object):
         robject = Rotation.from_matrix(rot_matrix)
         euler_angles = robject.as_euler('xyz', degrees=True)
         euler_angles = [0, 0, 0] 
+        if self.debug:
+            print('TRANSFORMED TO EULER Z\"Y\'X')
         return euler_angles
 
     def generate_json(self, rect, rot):
@@ -276,33 +306,39 @@ class Detect(object):
                round(rot[1], 6),
                round(rot[2], 6),
                round(rot[3], 6)]
-        jl = [[[int(rect[0]*1000),
-                int(rect[1]*1000),
-                int(rect[2]*1000)],
-               [rot[0], rot[1], rot[2]],
-               rot[3]]]
-        print(jl)
+        rect = np.multiply(1000,rect)
+        rect = np.around(rect).astype(int)
+        jl = [
+            [
+                [rect[0], rect[1], rect[2]],
+                [rot[0], rot[1], rot[2]],
+                rot[3]
+            ]
+        ]
+        if self.debug:
+            print('GENERATED JSON: ', jl)
         return json.dumps(jl).encode('ascii')
 
     def generate_xml(self, rect, rot):
         if len(rot) == 4:
             rot = Rotation.from_quat(rot).as_euler('XYZ', degrees=True)
-        print('rot :', rot)
+            print('EULER ANGLE: ', rot)
+        rect = np.multiply(1000,rect)
+        rect = np.around(rect).astype(int)
+        rot = np.around(rot, 6)
         robot_data = Element('Kinect')
         pattern = SubElement(robot_data,
                              'Pattern',
                              {'P':'0'})
         pattern.text = ' '
-        rect = np.multiply(1000,rect)
-        rect = np.around(rect).astype(int)
         coor_xml = SubElement(robot_data,
                                'Origin1',
                                {'X':str(rect[0]),
                                 'Y':str(rect[1]),
                                 'Z':str(rect[2]),
-                                'A':str(round(rot[2], 6)),
-                                'B':str(round(rot[1], 6)),
-                                'C':str(round(rot[0], 6)),
+                                'A':str(rot[2]),
+                                'B':str(rot[1]),
+                                'C':str(rot[0]),
                                 'BAG':'0'})
         coor_xml.text = ' '
         return ElementTree.tostring(robot_data, encoding='ascii')
@@ -310,13 +346,18 @@ class Detect(object):
     def transform_robot_base(self, rect):
         if rect.ndim != 1:
             return self.transform_robot_base(np.squeeze(rect))
+#        coor = np.asarray([-rect[1], -rect[0], -rect[2]])
+#        coor = np.asarray([rect[2], -rect[0], -rect[1]])
         coor = np.asarray([0, -rect[0], rect[1]])
-        print('tvec \n', coor)
+        if self.debug:
+            print('TRANSLATION VECTOR RELATIVE TO ROBOT: \n', coor)
+#        refp = [-0.139, 0.02046, -0.5125]
         refp = [0, 0, 0]
         coor = np.asarray([refp[0] * np.sign(coor[0]) + 1.0 * np.sign(coor[0]) * (abs(coor[0]) - refp[0]),
                            refp[1] * np.sign(coor[1]) + 1.0 * np.sign(coor[1]) * (abs(coor[1]) - refp[1]),
                            refp[2] * np.sign(coor[2]) + 1.0 * np.sign(coor[2]) * (abs(coor[2]) - refp[2])])
-        #print('scaled tvec \n', coor)
+        if self.debug:
+            print('SCALED TRANSLATION VECTOR RELATIVE TO ROBOT: \n', coor)
         return coor
 
     def send2robot(self, a_tvec, euler_angles):
@@ -324,8 +365,9 @@ class Detect(object):
             robot_data = self.generate_xml(a_tvec, euler_angles)
         elif self.robot == 'ABB':
             robot_data = self.generate_json(a_tvec, euler_angles)
-        print(robot_data)
+        print('SENDING DATA TO ROBOT.........')
         self.sock.send(robot_data)
+        print('DATA SENT TO ROBOT: \n', robot_data)
 
     def main(self):
         self.initialize_output()
@@ -338,7 +380,7 @@ class Detect(object):
         for path, im0s, vid_cap in dataset:
             t = time.time()
             if self.webcam:
-                p, self.im0 = path[-1], im0s[-1]
+                p, self.im0 = path[0], im0s[0]
             else:
                 p, self.im0 = path, im0s
             self.save_path = str(Path(self.out) / Path(p).name)
@@ -353,16 +395,16 @@ class Detect(object):
             g_rects = self.detect(img, model, colors)
 
             if self.view_img:
-                self.put_status_bar(t0)
+                self.put_status_bar()
                 cv2.imshow(p, self.im0)
                 if cv2.waitKey(1) & 0xFF == ord('q'): 
                     break
             if self.save_img:
                 if dataset.mode != 'images':
-                    self.put_status_bar(t0)
-                self.save_to_out(dataset.mode, vid_cap)
+                    self.put_status_bar()
+                self.save_to_out(vid_cap)
             if len(g_rects) != 0:
-                max_rect, found = self.get_xyedge_pos(g_rects)
+                max_rect = self.get_xyedge_pos(g_rects)
                 rvec, tvec = self.transform_pnp(max_rect)
                 euler_angles = self.transform_euler_angles(rvec)
                 a_tvec = self.transform_robot_base(tvec)
@@ -373,28 +415,32 @@ class Detect(object):
                 self.fps = 1/(time.time() - t)
             else:
                 self.fps += (1 * 10 ** -1)*(1/(time.time() - t) - self.fps)
-        print("elapsed time: {:.2f}".format(time.time() - t0))
-        print("approx. FPS: {:.2F}".format(self.fps))
+            print('COMPLETED IN {0} seconds'.format(time.time()-t))
+        if self.save_img:
+            print('SAVED VIDEO TO: {0}/sample.mp4'.format(self.out))
+        print("ELAPSED TIME: {:.2f}".format(time.time() - t0))
+        print("APPROXIMATE FPS: {:.2F}".format(self.fps))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp-1cls.cfg', help='cfg file path')
     parser.add_argument('--data', type=str, default='data/egg_tray.data', help='data file path')
-    parser.add_argument('--weights', type=str, default='weights/egg_tray.pt', help='path to weights file')
-    parser.add_argument('--source', type=str, default='0', help='source')  
+    parser.add_argument('--weights', type=str, default='weights/bestegg_tray_precision3.pt', help='path to weights file')
+    parser.add_argument('--source', type=str, default='0', help='source')  # input file/folder, 0 for webcam
     parser.add_argument('--camera-params', type=str, default='camera_data.npy', help='intrinsic camera parameters path')
-    parser.add_argument('--output', type=str, default='output', help='output folder')  
+    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
     parser.add_argument('--nms-thres', type=float, default=0.5, help='iou threshold for non-maximum suppression')
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--half', action='store_false', help='half precision FP16 inference')
     parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
-    parser.add_argument('--robot', default='KUKA', help='robot type')
     parser.add_argument('--view-img', action='store_false', help='display results')
     parser.add_argument('--disable-socket', action='store_false', help='disable socket')
     parser.add_argument('--test-socket', action='store_true', help='test socket on dummy server')
     parser.add_argument('--undistort', action='store_true', help='undistort images')
+    parser.add_argument('--save', action='store_true', help='save video')
+    parser.add_argument('--silent', action='store_false', help='no debug')
     opt = parser.parse_args()
     print(opt)
     with torch.no_grad():